b1ff1061320f141574b7adaa50655db0515e666d
Alexander Annenkov <alexander@iMac-Alexander.local>
2024-12-21 10:38:22 +0300

    first props add

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,13 +1,15 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { ArcticleParamsFormType } from './types';
 
 import styles from './ArticleParamsForm.module.scss';
+import clsx from 'clsx';
 
-export const ArticleParamsForm = () => {
+export const ArticleParamsForm = ({isOpen}: ArcticleParamsFormType = {isOpen:false}) => {
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isOpen} onClick={() => {}} />
+			<aside className={clsx([styles.container, isOpen && styles.containerOpen])}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
